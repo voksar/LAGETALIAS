b363763e409eddf2816581442276b14477f18528
Jonathan <63061970+voksar@users.noreply.github.com>
2024-09-09 20:55:27 +0200

    Add files via upload

---
+++ core.lua
@@ -0,0 +1,208 @@
+local aliasNames = {}
+-- Zyget --
+aliasNames["Zygetdh"] = "Zyget"
+aliasNames["Zygetdhh"] = "Zyget"
+aliasNames["Zygetw"] = "Zyget"
+aliasNames["Zygethd"] = "Zyget"
+aliasNames["Zygetww"] = "Zyget"
+aliasNames["Zygetdk"] = "Zyget"
+aliasNames["Zygetlyret"] = "Zyget"
+aliasNames["Zygethdd"] = "Zyget"
+
+-- LOGIK --
+
+aliasNames["Logikbaat"] = "Logik"
+aliasNames["Logikitten"] = "Logik"
+aliasNames["Logikgyat"] = "Logik"
+aliasNames["Logikfrog"] = "Logik"
+aliasNames["Logikxd"] = "Logik"
+aliasNames["Loogik"] = "Logik"
+aliasNames["Logiik"] = "Logik"
+
+-- YE --
+aliasNames["Yedh"] = "Ye"
+aliasNames["Yepc"] = "Ye"
+aliasNames["Yedk"] = "Ye"
+aliasNames["Yewarr"] = "Ye"
+
+-- SÅP --
+aliasNames["Såpshaman"] = "Såp"
+aliasNames["Sååp"] = "Såp"
+aliasNames["Såp"] = "Såp"
+aliasNames["Såpussy"] = "Såp"
+aliasNames["Såpan"] = "Såp"
+aliasNames["Såpuwu"] = "Såp"
+
+-- SNOW --
+aliasNames["Snowm"] = "Snow"
+aliasNames["Snowewl"] = "Snow"
+aliasNames["Snowevo"] = "Snow"
+aliasNames["Snowmagus"] = "Snow"
+aliasNames["Snowpdog"] = "Snow"
+
+-- SKAMENMANNEN --
+aliasNames["Skamenheimer"] = "Skame"
+aliasNames["Skaragon"] = "Skame"
+
+-- GNOPPE --
+aliasNames["Gnappoker"] = "Gnoppe"
+aliasNames["Gnappetwo"] = "Gnoppe"
+aliasNames["Gnappe"] = "Gnoppe"
+aliasNames["Gnappipapi"] = "Gnoppe"
+aliasNames["Gnapped"] = "Gnoppe"
+aliasNames["Gnappadin"] = "Gnoppe"
+aliasNames["Gnapazim"] = "Gnoppe"
+aliasNames["Gnappriest"] = "Gnoppe"
+
+-- NORSKEN --
+aliasNames["Nørsken"] = "Nørsken"
+aliasNames["Nørskendh"] = "Nørsken"
+aliasNames["Nørskenv"] = "Nørsken"
+aliasNames["Nørskenxd"] = "Nørsken"
+aliasNames["Nørskenxdd"] = "Nørsken"
+
+-- MATTUS --
+aliasNames["Muckfattus"] = "Mattus"
+aliasNames["Mattustvå"] = "Mattus"
+aliasNames["Mattus"] = "Mattus"
+aliasNames["Mattuggla"] = "Mattus"
+
+-- OLIVER --
+aliasNames["Imdatguy"] = "Oliver"
+aliasNames["Chkchkboom"] = "Oliver"
+aliasNames["Gigamonke"] = "Oliver"
+aliasNames["Chocondese"] = "Oliver"
+aliasNames["Succondese"] = "Oliver"
+
+-- JOHAN --
+aliasNames["Yungjohan"] = "Johan"
+aliasNames["Jooebiden"] = "Johan"
+aliasNames["Johanb"] = "Johan"
+aliasNames["Johanaura"] = "Johan"
+aliasNames["Johandh"] = "Johan"
+aliasNames["Johanarg"] = "Johan"
+
+-- GUSTAV --
+aliasNames["Enhealtack"] = "Gustav"
+aliasNames["Tvåhealstack"] = "Gustav"
+aliasNames["Kylianmbappé"] = "Gustav"
+aliasNames["Nejnejva"] = "Gustav"
+
+-- ALFRED --
+aliasNames["Froxïs"] = "Froxis"
+aliasNames["Froxisdudu"] = "Froxis"
+aliasNames["Froxïzz"] = "Froxis"
+aliasNames["Froxiz"] = "Froxis"
+aliasNames["Froxrizz"] = "Froxis"
+aliasNames["Proxis"] = "Froxis"
+aliasNames["Froxìs"] = "Froxis"
+
+-- EMIL --
+aliasNames["Flodasza"] = "Flodas"
+aliasNames["Yungflodas"] = "Flodas"
+aliasNames["Flodazsa"] = "Flodas"
+aliasNames["Flodaszax"] = "Flodas"
+aliasNames["Flodasåtta"] = "Flodas"
+
+-- FELIX --
+aliasNames["Feblizard"] = "Feb"
+aliasNames["Febrizz"] = "Feb"
+aliasNames["Feblizkard"] = "Feb"
+aliasNames["Augtistisk"] = "Feb"
+aliasNames["Lilfeb"] = "Feb"
+
+-- OGGE --
+aliasNames["Shamilli"] = "Sham"
+aliasNames["Shamgrip"] = "Sham"
+aliasNames["Shamwarr"] = "Sham"
+aliasNames["Shamstab"] = "Sham"
+
+-- DOLANZ --
+aliasNames["Dolans"] = "Dolanz"
+aliasNames["Dolandk"] = "Dolanz"
+aliasNames["Dolanp"] = "Dolanz"
+aliasNames["Dolzi"] = "Dolanz"
+
+-- CHARMIE --
+aliasNames["Charmieh"] = "Charmie"
+aliasNames["Yungjarmo"] = "Charmie"
+aliasNames["Charmiebow"] = "Charmie"
+aliasNames["Jarmotrap"] = "Charmie"
+aliasNames["Charmiedh"] = "Charmie"
+aliasNames["Charmiem"] = "Charmie"
+
+-- ANDREAS --
+aliasNames["Adnymonk"] = "Adny"
+aliasNames["Adnydin"] = "Adny"
+aliasNames["Adkny"] = "Adny"
+aliasNames["Adkndy"] = "Adny"
+aliasNames["Adnymonka"] = "Adny"
+aliasNames["Adndy"] = "Adny"
+
+-- JONTE --
+aliasNames["Jnote"] = "Jonte"
+aliasNames["Jnotewl"] = "Jonte"
+aliasNames["Fortnitexd"] = "Jonte"
+aliasNames["Jnoteaug"] = "Jonte"
+aliasNames["Yungjnote"] = "Jonte"
+
+-- RADIO --
+aliasNames["Meowdio"] = "Radio"
+aliasNames["Vokeradio"] = "Radio"
+aliasNames["Yungradio"] = "Radio"
+aliasNames["Radiobeacon"] = "Radio"
+aliasNames["Radiodeux"] = "Radio"
+aliasNames["Holypalasui"] = "Radio"
+aliasNames["Radiopriest"] = "Radio"
+aliasNames["Radiochi"] = "Radio"
+
+-- MIKE --
+aliasNames["Mikaeld"] = "Mike"
+aliasNames["Mikaeldk"] = "Mike"
+aliasNames["Mikekrigare"] = "Mike"
+aliasNames["Mikeal"] = "Mike"
+aliasNames["Mikeael"] = "Mike"
+aliasNames["Vindraserim"] = "Mike"
+aliasNames["Mikeald"] = "Mike"
+aliasNames["Mikaelm"] = "Mike"
+
+-- MAX --
+aliasNames["Maxwl"] = "Max"
+aliasNames["Maxwlb"] = "Max"
+aliasNames["Maxwlc"] = "Max"
+aliasNames["Maxwla"] = "Max"
+aliasNames["Maxly"] = "Max"
+aliasNames["Lillasyrra"] = "Max"
+aliasNames["Maxwblue"] = "Max"
+
+-- JOAKIM --
+aliasNames["Jockewarr"] = "Jocke"
+aliasNames["Jockekrigare"] = "Jocke"
+aliasNames["Joakimwarr"] = "Jocke"
+aliasNames["Larskrigare"] = "Jocke"
+aliasNames["Jockepriest"] = "Jocke"
+
+-- GUB -- 
+aliasNames["Gubdeux"] = "Gub"
+aliasNames["Imgubdup"] = "Gub"
+aliasNames["Yunggub"] = "Gub"
+aliasNames["Tchubz"] = "Gub"
+aliasNames["Tchooga"] = "Gub"
+aliasNames["Hungrycrab"] = "Gub"
+aliasNames["Tchubs"] = "Gub"
+aliasNames["Tchub"] = "Gub"
+aliasNames["Tchubpi"] = "Gub"
+aliasNames["Tgubb"] = "Gub"
+
+-- CJ --
+aliasNames["Cjzz"] = "Cj"
+aliasNames["Cjzh"] = "Cj"
+
+
+-- Voksar --
+aliasNames["Skurkvogge"] = "Voksar"
+aliasNames["Letmebangbrø"] = "Voksar"
+aliasNames["Vokzar"] = "Voksar"
+aliasNames["Voksar"] = "Voksar"
+
+_G["LAGETALIAS"] = aliasNames
